d197de8b4320ab0d7f414edc0aeab7a9766399d1
jaguar6105 <gilljoseph603@gmail.com>
2020-09-02 12:33:26 -0400

    add questions for letters, numbers, and symbols

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var uppercase = lowercase.toLocaleUpperCase();
 var numbers = "1234567890";
 var symbols = "!@#$%^&*()_-+=?/><`~,.";
 
-console.log(uppercase);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,8 +15,43 @@ function writePassword() {
 
 }
 
+//generates the password
 function generatePassword() {
-  return "a";
+  return   questionsAsked();
+}
+
+//asks the question about strings
+function questionsAsked() {
+  var sizeOfString = "";
+
+  var confirmThings = confirm("Do you want lowercase letters?");
+  if(confirmThings)
+  {
+    sizeOfString = lowercase;
+  }
+
+
+  confirmThings = confirm("Do you want uppercase letters?");
+  if(confirmThings)
+  {
+    sizeOfString = sizeOfString + uppercase;
+  }
+
+
+  confirmThings = confirm("Do you want numbers?");
+  if(confirmThings)
+  {
+    sizeOfString = sizeOfString + numbers;
+  }
+
+
+  confirmThings = confirm("Do you want special characters?");
+  if(confirmThings)
+  {
+    sizeOfString = sizeOfString + symbols;
+  }
+
+  return sizeOfString;
 }
 
 // Add event listener to generate button
